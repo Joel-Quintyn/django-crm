9a908ca608089217dc32847a47bb1565d769c4e5
Joel Quintyn <48963062+Joel-Quintyn@users.noreply.github.com>
2021-05-03 23:20:19 -0400

    Added Update & Delete Lead Veiws

--- leads/forms.py
+++ leads/forms.py
@@ -11,7 +11,7 @@ class LeadModelForm(forms.ModelForm):
          'agent',
       )
 
-# class LeadForm(forms.Form):
-#    first_name = forms.CharField()
-#    last_name = forms.CharField()
-#    age = forms.IntegerField(min_value=0)
+class LeadForm(forms.Form):
+   first_name = forms.CharField()
+   last_name = forms.CharField()
+   age = forms.IntegerField(min_value=0)

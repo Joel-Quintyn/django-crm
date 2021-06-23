1a529faf6e2fa3f8e0e667ff6332ef7d4fcbdb1c
Joel Quintyn <48963062+Joel-Quintyn@users.noreply.github.com>
2021-06-22 23:16:06 -0400

    Initiated static files & updated templates

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
@@ -11,3 +13,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', include('leads.urls', namespace="leads"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
